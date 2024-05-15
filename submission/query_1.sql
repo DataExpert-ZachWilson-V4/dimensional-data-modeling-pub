56099e253cda4329a941e06d4038c4974174222a
mposada97 <85133902+mposada97@users.noreply.github.com>
2024-05-15 17:51:13 -0500

    Update query_1.sql
    Resubmitting. This creates a cumulative table for actors.

---
+++ submission/query_1.sql
@@ -0,0 +1,18 @@
+CREATE TABLE mposada.actors (
+    actor VARCHAR,
+    actor_id VARCHAR,
+    films ARRAY(ROW(  --STRUCT ARRAY FOR ALL FILMS OF CORRESPONDING ACTOR UP TO CURRENT YEAR
+        film VARCHAR,
+        votes INT,
+        rating DOUBLE,
+        film_id VARCHAR
+    )),
+    quality_class VARCHAR,
+    is_active BOOLEAN,
+    current_year INT
+)
+WITH
+  (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['current_year']  -- PARTITION BY CURRENT YEAR
+  )
