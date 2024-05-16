b4834318b62affe80dc8cad9ccf5b0968fec78f5
Hamzah Arif Qadeer <qadeer.hamzah97@gmail.com>
2024-05-16 19:39:39 -0400

    Update query_1.sql

---
+++ submission/query_1.sql
@@ -0,0 +1,21 @@
+-- query_1
+CREATE TABLE qhamzah.actors (
+  actor VARCHAR,
+  actor_id VARCHAR,
+  films ARRAY(
+    ROW(
+      film VARCHAR,
+      votes INTEGER,
+      rating DOUBLE,
+      film_id VARCHAR
+    )
+  ),
+  quality_class VARCHAR,
+  is_active BOOLEAN,
+  current_year INTEGER
+)
+WITH
+  (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['current_year']
+  )
