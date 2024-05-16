3d8b897768b6a0e00e28803d5f3cd471e0ceba27
Billy Switzer <47672463+billyswitzer@users.noreply.github.com>
2024-05-15 22:20:54 -0400

    Update query_1.sql
    DDL statement for actors table

---
+++ submission/query_1.sql
@@ -0,0 +1,14 @@
+CREATE TABLE actors
+(
+  actor VARCHAR,
+  actor_id VARCHAR,
+  films ARRAY(ROW(film VARCHAR, votes INTEGER, rating DOUBLE, film_id VARCHAR)),
+  quality_class VARCHAR,
+  is_active BOOLEAN,
+  current_year INTEGER 
+)
+WITH
+(
+  FORMAT = 'PARQUET',
+  PARTITIONING = ARRAY['current_year']
+)
