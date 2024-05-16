8598380c143aa918b478c496de150cc7de42aa76
meetapandit <meeta.pandit890@gmail.com>
2024-05-16 14:47:32 -0700

    initial submission to updated link

---
+++ submission/query_3.sql
@@ -0,0 +1,23 @@
+CREATE TABLE actors_history_scd(
+  -- attribute for actor name
+actor VARCHAR,
+  -- attribute for actor_id identifier
+actor_id VARCHAR,
+  -- categorical variable based on avg. rating 
+quality_class VARCHAR,
+  -- flag to track if actor is active in the industry
+is_active BOOLEAN,
+  -- start date for actor's time frame in industry
+start_date DATE,
+  -- end date for actor's time frame in industry
+end_date DATE,
+-- partitioning key column
+current_year INTEGER
+)
+
+WITH (
+  -- format to staore table in Iceberg
+  format = 'PARQUET',
+  --  partition on current_year to optimize retrieval
+  partitioning = ARRAY['current_year']
+  )
---
+++ submission/query_4.sql
@@ -0,0 +1,50 @@
+INSERT INTO actors_history_scd
+--Full load of actors_history_scd table by tracking changes in --is_active and quality_class
+WITH lagged AS (
+SELECT actor
+     , actor_id
+     , quality_class
+    , LAG(quality_class, 1) OVER(
+        PARTITION BY actor_id ORDER BY current_year) AS quality_class_last_year
+     , is_active
+     , LAG(is_active, 1) OVER(PARTITION BY actor, actor_id ORDER BY current_year) is_active_last_year
+     , current_year
+FROM meetapandit89096646.actors
+WHERE current_year <= 2020
+)
+, changed AS (
+  SELECT actor
+     , actor_id
+     , quality_class
+     , quality_class_last_year
+     , is_active
+     , is_active_last_year
+     -- group changes together
+     , SUM(CASE WHEN is_active <> is_active_last_year AND quality_class <> quality_class_last_year THEN 1 ELSE 0 END) OVER(PARTITION BY actor, actor_id ORDER BY current_year) AS status_changed
+     , current_year
+  FROM lagged
+)
+
+SELECT actor
+     , actor_id
+     , quality_class
+     , is_active
+     -- construct date from current_year and make it first day of year
+     , MIN(DATE_PARSE(CONCAT_WS('-', ARRAY[CAST(current_year AS VARCHAR),
+       '01',
+       '01']
+       ), '%Y-%m-%d')) AS start_date
+       -- construct end date from current_year and make it point to last day of year
+     , MAX(DATE_PARSE(CONCAT_WS('-',
+     ARRAY[CAST(current_year AS VARCHAR),
+     '12',
+     '31'
+     ]
+     ), '%Y-%m-%d')) AS end_date
+     , 2020 AS current_year
+FROM changed
+GROUP BY actor
+     , actor_id
+     , quality_class
+     , is_active
+     , status_changed
