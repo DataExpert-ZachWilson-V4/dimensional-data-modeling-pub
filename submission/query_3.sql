8b3a7b44ec5ff6b1b998da526bb108badee909bf
codetutelage <69841209+codetutelage@users.noreply.github.com>
2024-05-17 14:09:17 -0700

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,6 +1,6 @@
 /*
 Actors History SCD Table DDL (query_3)
-
+Prompt: 
 Write a DDL statement to create an actors_history_scd table that tracks the following fields for each actor in the actors table:
   * quality_class
   * is_active
