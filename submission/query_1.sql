f60be4a181264735c929af0714345605f383b870
Julie Scherer <schererjulie96@gmail.com>
2024-05-17 11:58:53 -0500

    fake query 1 and 2 submission for testing

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,9 +1,6 @@
-CREATE OR REPLACE TABLE actors (
+CREATE TABLE actors (
 actor VARCHAR,
 actor_id VARCHAR,
-films ARRAY(
-  ROW( year INTEGER, film VARCHAR, votes INTEGER, rating DOUBLE, film_id VARCHAR )
-),
 quality_class VARCHAR,
 is_active BOOLEAN,
 current_year INTEGER
---
+++ submission/query_2.sql
@@ -0,0 +1,6 @@
+INSERT INTO actors 
+'actor',
+'actor_id',
+'quality_class',
+false,
+2024
