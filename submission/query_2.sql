a10e459c0f34bc896e119a94534dc435c89689aa
Hamzah Arif Qadeer <qadeer.hamzah97@gmail.com>
2024-05-17 14:01:23 -0400

    Added comments to query_2.sql

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,12 +1,16 @@
 -- query_2
+
+-- Insert data into the 'actors' table using data accumulated so far from previous years and the current year
 INSERT INTO
   actors
 WITH
+  -- Subquery to select all actors from the previous years
   last_year AS (
     SELECT *
       FROM actors
      WHERE current_year = 1999
   ),
+  -- Subquery to select actor information from the upcoming year 2000
   this_year AS (
     SELECT 
       actor,
@@ -18,18 +22,19 @@ WITH
     WHERE year = 2000
     GROUP BY actor, actor_id, year
   )
-  
+
+-- Main query to insert data into the 'actors' table
 SELECT
   COALESCE(ly.actor, ty.actor) AS actor,
   COALESCE(ly.actor_id, ty.actor_id) AS actor_id,
   CASE
-    -- actor didn't star a movie this year
+    -- Actor didn't star in a movie this year
     WHEN ty.year IS NULL
       THEN ly.films
-    -- actor started their career
+    -- Actor started their career
     WHEN ty.year IS NOT NULL AND ly.films IS NULL
       THEN ty.films
-    -- actor continuing their career
+    -- Actor continued their career
     WHEN ty.year IS NOT NULL AND ly.films IS NOT NULL
       THEN ty.films || ly.films
   END AS films,
@@ -49,3 +54,4 @@ SELECT
   COALESCE(ty.year, ly.current_year+1) AS current_year
   FROM last_year ly
   FULL OUTER JOIN this_year ty ON ly.actor = ty.actor OR (ly.actor_id IS NULL AND ty.actor_id IS NOT NULL)
+  
