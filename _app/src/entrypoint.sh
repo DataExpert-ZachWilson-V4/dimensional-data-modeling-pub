b6dfee932a64e4f7c772537ee18cce6634854897
Julie Scherer <schererjulie96@gmail.com>
2024-05-17 12:23:13 -0500

    fixed edge case and formatting

--- _app/src/entrypoint.sh
+++ _app/src/entrypoint.sh
@@ -21,7 +21,7 @@ last_line=$(echo "$RETURN_VALUE" | tail -n 1)
 
 if [[ "$last_line" == 'All tests passed successfully' ]]; then
   echo "Tests passed! Great work! Generating feedback for changed files: $CHANGED_FILES..."
-  exec python src/generate_comment.py
+  python src/generate_comment.py
   echo "Done!"
 else
   echo "---------------------------------------------------------------------------------"
