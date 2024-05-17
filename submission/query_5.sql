ebe0377002c987c3d602597e4f78abd258b0d63a
codetutelage <69841209+codetutelage@users.noreply.github.com>
2024-05-17 00:18:38 -0700

    Update query_5.sql

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -1,3 +1,7 @@
+/*
+Actors History SCD Table Incremental Backfill Query (query_5)
+Write an "incremental" query that can populate a single year's worth of the actors_history_scd table by combining the previous year's SCD data with the new incoming data from the actors table for this year.
+*/
 insert into harathi.actors_history_scd
 with last_year As
 (
