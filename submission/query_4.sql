28a31116abc08abf0c2ed57bfe34b73e17441dfd
codetutelage <69841209+codetutelage@users.noreply.github.com>
2024-05-16 22:34:35 -0700

    Update query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -1,7 +1,7 @@
 /*
 Actors History SCD Table Batch Backfill Query (query_4)
 
-Write a "backfill" query that can populate the entire actors_history_scd table in a single qu
+Write a "backfill" query that can populate the entire actors_history_scd table in a single query
 */
 
 INSERT INTO harathi.actors_history_scd 
