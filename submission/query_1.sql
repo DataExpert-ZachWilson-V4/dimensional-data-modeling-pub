758990d59343746848ace44abd47421882c57b4a
Julie Scherer <schererjulie96@gmail.com>
2024-05-17 12:44:47 -0500

    fake query 1 submission with my schema name

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,10 +1,10 @@
-CREATE TABLE actors (
-actor VARCHAR,
-actor_id VARCHAR,
-quality_class VARCHAR,
-is_active BOOLEAN,
-current_year INTEGER
+CREATE TABLE juliescherer.actors (
+  actor VARCHAR,
+  actor_id VARCHAR,
+  quality_class VARCHAR,
+  is_active BOOLEAN,
+  current_year INTEGER
 ) WITH (
-FORMAT = 'PARQUET',
-partitioning = ARRAY ['current_year']
+  FORMAT = 'PARQUET',
+  partitioning = ARRAY ['current_year']
 )
