1b834110a193d49f98d1c8bdd6f014962e33fce7
Hamzah Arif Qadeer <qadeer.hamzah97@gmail.com>
2024-05-17 14:02:18 -0400

    Added comments to query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -1,6 +1,11 @@
 -- query_4
-INSERT INTO actors_history_scd
-WITH lagged AS (
+
+-- Insert data into the 'actors_history_scd' table using historical data from the 'actors' table
+INSERT INTO
+  actors_history_scd
+WITH
+  -- Subquery to calculate previous year's active status for each actor
+  lagged AS (
   SELECT
     actor,
     quality_class,
@@ -9,13 +14,16 @@ WITH lagged AS (
     current_year
     FROM actors
 ),
-streaked AS (
+  -- Subquery to identify streaks of activity/inactivity for each actor
+  streaked AS (
   SELECT
     *,
+	-- Identify streaks of changes in activity status
     SUM(CASE WHEN is_active <> is_active_last_year THEN 1 ELSE 0 END) OVER (PARTITION BY actor ORDER BY current_year) AS streak_identifier
     FROM lagged
 )
 
+-- Main query to insert data into 'actors_history_scd' table
 SELECT
   actor,
   MAX(quality_class) AS quality_class,
@@ -25,3 +33,4 @@ SELECT
   2000 AS current_year
   FROM streaked
  GROUP BY actor, streak_identifier
+ 
