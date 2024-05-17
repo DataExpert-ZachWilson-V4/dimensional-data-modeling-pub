198d43db5135948f4af28785a22fc34a0a22e198
codetutelage <69841209+codetutelage@users.noreply.github.com>
2024-05-16 22:01:57 -0700

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -9,6 +9,7 @@ Write a DDL statement to create an actors_history_scd table that tracks the fo
 Note that this table should be appropriately modeled as a Type 2 Slowly Changing Dimension Table (start_date and end_date).
 */
 
+
 CREATE TABLE harathi.actors_history_scd (
   actor_id VARCHAR,
   actor VARCHAR,
