c1b1781c5f1ca50fe9751f706cb27ca4afe91df8
derekleung0399 <147735897+derekleung0399@users.noreply.github.com>
2024-05-17 17:19:39 +0800

    Update query_3.sql (attempt 6)

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,5 +1,7 @@
 -- Note this table contains 2 SCDs: quality_class & is_active
 -- note start/end/current date is actually year, following the naming conventions of hw readme
+-- even though actor are not explicitly required, having the name of the actor could be beneficial for downstream users to refer with
+-- even though current_year are not explicitly required, having it would better record the incremental updating history
 
 CREATE TABLE derekleung.actors_history_scd (
   actor_id VARCHAR(9),
