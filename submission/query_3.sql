e8fdcbce0a1176d6b47065acb32bbc617e149def
Raj <raj@Rajs-MacBook-Air.local>
2024-05-16 23:48:12 -0400

    homework_week1

---
+++ submission/query_3.sql
@@ -0,0 +1,20 @@
+-- Create table to store historical data for actors using Slowly Changing Dimensions (SCD) approach
+CREATE OR REPLACE TABLE rajkgupta091041107.actors_history_scd (
+    -- Actor's ID
+    actor_id VARCHAR,
+    -- Quality class of the actor
+    quality_class VARCHAR,
+    -- Indicator for whether the actor is active
+    is_active BOOLEAN, 
+    -- Start date for the actor's status
+    start_date DATE, 
+    -- End date for the actor's status
+    end_date DATE, 
+    -- Date when the record was last modified
+    modified_date DATE 
+) WITH (
+    -- Specify the data format as PARQUET
+    format = 'PARQUET', 
+    -- Partition the table by the modified date for efficient querying
+    partitioning = ARRAY['modified_date'] 
+)
