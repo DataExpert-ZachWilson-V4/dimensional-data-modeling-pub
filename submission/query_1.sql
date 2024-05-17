4b35fad720d44e072b99f6b90295eb6990d68d6f
codetutelage <69841209+codetutelage@users.noreply.github.com>
2024-05-17 14:08:15 -0700

    Updating for submission

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -16,6 +16,7 @@ Write a DDL query to create an actors table with the following fields:
     * bad: Average rating ≤ 6.
 * is_active: A BOOLEAN field that indicates whether an actor is currently active in the film industry (i.e., making films this year).
 * current_year: The year this row represents for the actor
+
 **/
    
 CREATE TABLE harathi.actors (
@@ -33,7 +34,7 @@ CREATE TABLE harathi.actors (
       rating DOUBLE -- rating: The rating of the film.
     )
   ),
---quality_class: A categorical bucketing of the average rating of the movies for this actor in their most recent year
+  --quality_class: A categorical bucketing of the average rating of the movies for this actor in their most recent year
   quality_class varchar,
    --is_active: A BOOLEAN field that indicates whether an actor is currently active in the film industry
   is_active BOOLEAN,
