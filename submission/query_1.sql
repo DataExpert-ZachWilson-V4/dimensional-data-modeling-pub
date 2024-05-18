ffcfe74326556f49b71be05d3094ff809bc631cb
jonas-nothnagel <jonas.nothnagel@gmail.com>
2024-05-18 17:08:03 +0200

    changed query 1 and 2

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,34 +1,26 @@
-CREATE OR REPLACE Table nonasj.actors (
-  -- 'actor': Stores the actor's name.
-  actor VARCHAR,
-  -- 'actor_id': Unique identifier for each actor.
+CREATE or REPLACE TABLE nonasj.actors (
+  --actor: Actor name
+   actor VARCHAR,
+   --actor_id: Actor's ID
   actor_id VARCHAR,
-  -- 'films': Array of ROWs for multiple films associated with each actor. Each row contains film details.
+  -- films: An array of struct with the following fields:
   films ARRAY(
     ROW(
-      -- 'year': Release year of the film.
-      year INTEGER
-      -- 'film': Name of the film.
-      film VARCHAR,
-      -- 'votes': Number of votes the film received.
-      votes INTEGER,
-      -- 'rating': Rating of the film.
-      rating DOUBLE,
-      -- 'film_id': Unique identifier for each film.
-      film_id VARCHAR,
+      film VARCHAR, -- film: The name of the film.
+      film_id VARCHAR, --film_id: A unique identifier for each film.
+      year INTEGER, -- Year this grain belongs to
+      votes INTEGER, -- votes: The number of votes the film received.
+      rating DOUBLE -- rating: The rating of the film.
     )
   ),
-  -- 'quality_class': Categorical rating based on average rating in the most recent year.
-  quality_class VARCHAR,
-  -- 'is_active': Indicates if the actor is currently active.
+ --quality_class: A categorical bucketing of the average rating of the movies for this actor in their most recent year
+  quality_class varchar,
+   --is_active: A BOOLEAN field that indicates whether an actor is currently active in the film industry
   is_active BOOLEAN,
-  -- 'current_year': Represents the year this row is relevant for the actor.
+   --current_year: The year this row represents for the actor
   current_year INTEGER
 )
 WITH
   (
-    -- Data stored in PARQUET format for optimized analytics.
     FORMAT = 'PARQUET',
-    -- Partitioned by 'current_year' for efficient time-based analysis.
-    PARTITIONING = ARRAY['current_year']
-  )
+    partitioning = ARRAY['current_year'])
