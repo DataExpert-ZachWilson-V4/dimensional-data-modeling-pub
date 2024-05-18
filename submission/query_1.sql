16b8cb8512ab63732ee04a25dbafa10b9f6b78cd
SteveHut <stevith@gmail.com>
2024-05-18 13:50:54 -0400

    work on query 1 and 2

---
+++ submission/query_1.sql
@@ -0,0 +1,15 @@
+CREATE TABLE actors
+(
+  actor VARCHAR,
+  actor_ID VARCHAR,
+  films ARRAY(ROW
+  (
+    film VARCHAR,
+    votes INTEGER,
+    rating DOUBLE,
+    film_ID VARCHAR
+  )),
+  quality_class VARCHAR,
+  is_active BOOLEAN,
+  current_year INTEGER
+)
