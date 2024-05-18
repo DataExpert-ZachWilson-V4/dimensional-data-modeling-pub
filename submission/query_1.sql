d3682c2d41f2ce16c6ed829ca7590c428d97fd88
Nitin Tharwani <tharwaninitin182@gmail.com>
2024-05-19 02:28:56 +0530

    Update query_1.sql

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,21 +1,21 @@
 CREATE OR REPLACE TABLE tharwaninitin.actors (
-  actor VARCHAR,
-  actor_id VARCHAR,
-  films ARRAY(
-    ROW(
-      film VARCHAR,
-      votes INTEGER,
-      rating DOUBLE,
-      film_id VARCHAR,
-      year INTEGER
-    )
-  ),
-  quality_class VARCHAR,
-  is_active BOOLEAN,
-  current_year INTEGER
+    actor VARCHAR, -- Name of the actor
+    actor_id VARCHAR, -- Unique identifier for each actor
+    films ARRAY( -- Array of films the actor has participated in
+        ROW(
+            film VARCHAR, -- Name of the film
+            votes INTEGER, -- Number of votes the film has received
+            rating DOUBLE, -- Rating of the film
+            film_id VARCHAR, -- Unique identifier for each film
+            year INTEGER -- Release year of the film
+        )
+    ),
+    quality_class VARCHAR, -- Classification of the actor's performance quality
+    is_active BOOLEAN, -- Indicates whether the actor is currently active
+    current_year INTEGER -- Year in which the data is current
 )
 WITH
-  (
-    FORMAT = 'PARQUET',
-    PARTITIONING = ARRAY['current_year']
-  )
+(
+    FORMAT = 'PARQUET', -- Data format for storage
+    partitioning = ARRAY['current_year'] -- Partitioning key for optimization
+)
