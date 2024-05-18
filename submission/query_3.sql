4b437ac67a45f10daf8234e9904761a4ef628944
chinmay <chinmay.hebbal@pocketfm.com>
2024-05-19 04:47:12 +0530

    changing the data type for table

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,14 +1,15 @@
 CREATE OR REPLACE TABLE chinmay_hebbal.actors_history_scd (
-    actor VARCHAR ,           -- actor name
-    is_active BOOLEAN,                -- Flag to indicate if the actor is currently active in the film industry
-    average_rating DOUBLE,            -- column to store the average rating
-    quality_class VARCHAR, --column to store the average rating category
-    start_date INTEGER,               -- Start date and keeping it Integer to match the current_year column's datatype
-    end_date INTEGER,                 -- End date and keeping it Integer to match the current_year column's datatype
-    current_year INTEGER              -- For partitioning strategy
+  actor VARCHAR,
+  actor_id VARCHAR,
+  is_active BOOLEAN,
+  average_rating DOUBLE, -- column to store the average rating
+  quality_class VARCHAR, --column to store the average rating category
+  start_date INTEGER,
+  end_date INTEGER,
+  current_year INTEGER
 )
 WITH
   (
-    FORMAT = 'PARQUET',
-    partitioning = ARRAY['current_year']   -- Partitioning on current_year for query efficiency
+    FORMAT = 'PARQUET', -- the format that we would like to store the data
+    partitioning = ARRAY['current_year'] -- partitions the table by the current film year
   )
