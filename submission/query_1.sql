15ef433e5e744841c76dd14f9dc9974c00f83446
fayiz_tk <fayiz.tk@zoom.us>
2024-05-18 19:17:12 +0530

    query1

---
+++ submission/query_1.sql
@@ -0,0 +1,20 @@
+create table fayiztk.actors (
+    actor varchar,
+    actor_id varchar,
+    films ARRAY (
+        ROW (
+            film varchar,
+            votes integer,
+            rating double,
+            film_id varchar
+        )
+    ),
+    quality_class varchar,
+    is_active boolean,
+    current_year integer
+)
+with  
+(  
+format='parquet',  
+partitioning=ARRAY['current_year']  
+) 
