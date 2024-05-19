cb1e334f745739ec7c914b2863109391f5844cb6
hariomnayani <47569156+hariomnayani@users.noreply.github.com>
2024-05-19 17:26:52 -0400

    Update query_1.sql

---
+++ submission/query_1.sql
@@ -0,0 +1,20 @@
+--Actors Table DDL (query_1)
+--DESCRIBE bootcamp.actor_films : I used this query to find the datatype of each column in the master_table
+create table hariomnayani88482.actors(
+      actor varchar,	
+      actor_id varchar,	
+      films ARRAY(ROW(
+        film varchar,
+        votes integer,
+        rating double,
+        film_id varchar
+      )),
+      quality_class varchar,
+      is_active boolean,
+      current_year integer      
+)
+WITH
+  (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['current_year']
+  )
