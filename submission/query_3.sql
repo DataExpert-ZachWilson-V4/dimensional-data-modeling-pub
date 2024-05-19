b9cc803ac5b7ca57bdffd92ad6ab795aa38de118
Donny <168041002+xMidnightSoun@users.noreply.github.com>
2024-05-18 22:17:12 -0500

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,5 +1,4 @@
-DROP TABLE dswills94.actors_history_scd
-CREATE TABLE dswills94.actors_history_scd (
+CREATE OR REPLACE TABLE dswills94.actors_history_scd (
 	actor VARCHAR,
 	actor_id VARCHAR,
 	quality_class VARCHAR,
