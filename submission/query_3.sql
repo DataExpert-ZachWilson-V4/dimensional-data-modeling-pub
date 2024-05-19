66cea0bf7082ab9521509125be18fc4b85b7b106
SteveHut <shutwelker@earthres.com>
2024-05-19 13:06:40 -0400

    commits for query_3 and 4

---
+++ submission/query_3.sql
@@ -0,0 +1,10 @@
+-- DDL statement to creat an 'actors_history_scd'
+
+CREATE TABLE actors_history_scd (
+    actor VARCHAR,
+    actor_id VARCHAR,
+    quality_class VARCHAR,
+    is_active BOOLEAN,
+    start_date INTEGER,
+    end_date INTEGER
+)
