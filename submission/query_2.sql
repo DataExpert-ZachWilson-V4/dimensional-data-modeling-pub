64a5fce3331af28accca0e67f12050e36eb77849
Ivomuk <83593267+Ivomuk@users.noreply.github.com>
2024-05-19 14:04:15 +0300

    Update query_2.sql

---
+++ submission/query_2.sql
@@ -0,0 +1,49 @@
+insert into ivomuk37854.actors
+WITH previous_year AS (
+select * from ivomuk37854.actors
+ where current_year = 1960
+),
+this_year AS (
+ select   
+    actor,
+    actor_id,
+    ARRAY_AGG(
+      ROW(
+       film,
+        film_id,
+        votes,
+        rating,        
+        year
+      )
+    ) AS films,
+    AVG(rating) AS avg_rating,
+    year
+  FROM
+    bootcamp.actor_films
+  WHERE
+    rating is not null
+    and year = 1961
+  GROUP BY
+    actor,
+    actor_id,
+    year 
+)
+select  
+  COALESCE(py.actor, ty.actor) AS actor,
+  COALESCE(py.actor_id, ty.actor_id) AS actor_id,
+CASE 
+WHEN ty.films IS NULL THEN py.films
+WHEN ty.films IS NOT NULL and py.films IS NULL
+THEN ty.films
+WHEN ty.films IS NOT NULL and py.films IS NOT NULL THEN ty.films || py.films
+END AS films,
+CASE WHEN avg_rating > 8 THEN 'star'
+     WHEN avg_rating > 7 THEN 'good'
+     WHEN avg_rating > 6 THEN 'average'
+     ELSE 'bad'
+END AS quality_class,
+ty.year IS NOT NULL AS is_active, 
+COALESCE(ty.year, py.current_year + 1) AS current_year
+from previous_year py
+ FULL OUTER JOIN this_year ty
+ ON py.actor_id = ty.actor_id
