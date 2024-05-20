ac6dea2f40fdeb385c66f31e3d2af0570ae5c540
Vladislav Chebotarev <vladislav.chebotarev@metricalo.com>
2024-05-21 02:32:44 +0400

    One more try

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,26 +1,18 @@
-CREATE
-OR REPLACE TABLE  halloweex.actors (
+CREATE OR REPLACE TABLE halloweex.actors (
     actor VARCHAR NOT NULL,
     actor_id VARCHAR NOT NULL,
-    -- Array for multiple films associated with each actor. Each row contains film details.
-    films ARRAY(
-        ROW(
-            year INTEGER,
-            film VARCHAR,
-            votes INTEGER,
-            rating DOUBLE,
-            film_id VARCHAR
-        )
-    ),
-    -- Categorical rating based on average rating in the most recent year
+    films ARRAY<ROW(
+        year INTEGER,
+        film VARCHAR,
+        votes INTEGER,
+        rating DOUBLE,
+        film_id VARCHAR
+    )>,
     quality_class VARCHAR,
-    -- Indicates if the actor is currently active
     is_active BOOLEAN,
-    -- Represents the year this row is relevant for the actor
     current_year INTEGER
 )
-WITH
-    (
-        FORMAT = 'PARQUET',
-        partitioning = ARRAY['current_year']
-    );
+WITH (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['current_year']
+);
