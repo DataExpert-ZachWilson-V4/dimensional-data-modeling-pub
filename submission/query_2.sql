9b9801883c5e53bfe27180d727d9973bc0220c94
Alissa Del Toro <34179808+alissadeltoro@users.noreply.github.com>
2024-05-20 00:30:09 -0400

    Update query_2.sql
    Adjust the quality_class calculation based on that year or the year prior vs. all ratings from actor from all time

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,31 +1,26 @@
-INSERT INTO alissabdeltoro.actors
+INSERT INTO alissabdeltoro.actors (actor, actor_id, films, quality_class, is_active, current_year)
+
+
 -- Common Table Expression (CTE) to fetch data for the last year
 WITH last_year AS (
     SELECT * 
     FROM alissabdeltoro.actors
-    WHERE current_year = 2017  
-    AND actor IN ('Lance Henriksen', 'William Shatner')
+    WHERE current_year = 2019
 ),
 -- Common Table Expression (CTE) to fetch data for the current year and calculate average ratings
 this_year AS (
     SELECT 
         actor,
         actor_id,
+        AVG(rating) AS avg_rating,
         ARRAY_AGG(ROW(film, votes, rating, film_id)) AS films,
         year AS current_year  
     FROM bootcamp.actor_films
-    WHERE year = 2018  
+    WHERE year = 2020
     GROUP BY actor, actor_id, year
-),
--- Common Table Expression (CTE) to calculate average ratings for actors in the current year
-avg_ratings AS (
-    SELECT 
-        actor_id,
-        AVG(rating) AS avg_rating
-    FROM bootcamp.actor_films
-    GROUP BY actor_id
 )
--- Main query to select and manipulate the data
+
+-- Inserting the results into the alissabdeltoro.actors table
 SELECT 
     COALESCE(ly.actor, ty.actor) AS actor,
     COALESCE(ly.actor_id, ty.actor_id) AS actor_id,
@@ -35,13 +30,14 @@ SELECT
         WHEN (ty.current_year IS NOT NULL AND ly.films IS NOT NULL) THEN ty.films || ly.films  
     END AS films,
     CASE
-        WHEN ar.avg_rating > 8 THEN 'star'  
-        WHEN ar.avg_rating > 7 THEN 'good'  
-        WHEN ar.avg_rating > 6 THEN 'average'  
-        ELSE 'bad'  
+        WHEN ty.avg_rating > 8 THEN 'star'  
+        WHEN ty.avg_rating > 7 THEN 'good'  
+        WHEN ty.avg_rating > 6 THEN 'average' 
+        WHEN ty.avg_rating <= 6 THEN 'bad' 
+        WHEN ly.quality_class IS NOT NULL THEN ly.quality_class
+        ELSE NULL
     END AS quality_class,
     ty.current_year IS NOT NULL AS is_active,
     COALESCE(ty.current_year, ly.current_year + 1) AS current_year
 FROM last_year ly
-FULL OUTER JOIN this_year ty ON ly.actor_id = ty.actor_id  
-LEFT JOIN avg_ratings AS ar ON COALESCE(ly.actor_id, ty.actor_id) = ar.actor_id
+FULL OUTER JOIN this_year ty ON ly.actor_id = ty.actor_id
