2a1532630a2d2a9265b95939c794954fc26de448
Alissa Del Toro <34179808+alissadeltoro@users.noreply.github.com>
2024-05-20 17:55:44 -0400

    Update query_1.sql
    Added year into films array

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -3,10 +3,11 @@ CREATE TABLE alissabdeltoro.actors (
     actor VARCHAR,  -- Name of the actor
     actor_id VARCHAR,  -- Unique identifier for the actor
     films ARRAY(ROW(
+        year INTEGER, -- 'year': Release year of the film
         film VARCHAR, 
         votes INTEGER, 
         rating DOUBLE, 
-        film_id VARCHAR
+        film_id VARCHAR -- Unique identifier for the film
     )),  -- Array of films with their details
     quality_class VARCHAR,  -- Categorical bucketing of the average rating of the movies
     is_active BOOLEAN,  -- Indicates whether an actor is currently active
