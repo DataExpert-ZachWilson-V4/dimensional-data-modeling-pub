ef769b8a6149d00305f938287f34dd3aa418f935
Vladislav Chebotarev <vladislav.chebotarev@metricalo.com>
2024-05-21 01:56:38 +0400

    Update queries in submission

---
+++ submission/query_1.sql
@@ -0,0 +1,25 @@
+CREATE TABLE actors (
+    actor VARCHAR NOT NULL,
+    actor_id VARCHAR NOT NULL,
+    -- Array for multiple films associated with each actor. Each row contains film details.
+    films ARRAY(
+        ROW(
+            year INTEGER,
+            film VARCHAR,
+            votes INTEGER,
+            rating DOUBLE,
+            film_id VARCHAR
+        )
+    ),
+    -- Categorical rating based on average rating in the most recent year
+    quality_class VARCHAR,
+    -- Indicates if the actor is currently active
+    is_active BOOLEAN,
+    -- Represents the year this row is relevant for the actor
+    current_year INTEGER
+)
+WITH
+    (
+        FORMAT = 'PARQUET',
+        partitioning = ARRAY['current_year']
+    );
---
+++ submission/query_3.sql
@@ -0,0 +1,13 @@
+CREATE TABLE halloweex.actors_history_scd (
+    actor VARCHAR NOT NULL,
+    quality_class VARCHAR,
+    start_date INTEGER,
+    end_date INTEGER,
+    current_year INTEGER,
+    is_current BOOLEAN,
+    is_active BOOLEAN
+)
+WITH (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['current_year']
+)
