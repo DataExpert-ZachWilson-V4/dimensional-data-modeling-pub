7a68bcf24debf653db6165ae58e6e972c570966e
Shruthi <shruthi0898>
2024-05-21 00:27:18 -0700

    Query 1

---
+++ submission/query_1.sql
@@ -0,0 +1,22 @@
+-- Creating table actors from the fields in actors_films dataset
+
+CREATE TABLE shruthishridhar.actors (
+    actor VARCHAR,  -- actor's name
+    actor_id VARCHAR, -- actor's unique identifier
+    films ARRAY(  -- actor's films array with a struct for each film
+      ROW(
+        film VARCHAR, -- film name
+        votes INTEGER,  -- number of votes this film received
+        rating DOUBLE,  -- rating for the film
+        film_id VARCHAR, -- film's unique identifier
+        year INTEGER  -- year in which the film released
+      )
+    ),
+    quality_class VARCHAR,  -- bucketing of average rating of films the most recent year
+    is_active BOOLEAN,  -- indicates if the actor is currently active
+    current_year INTEGER  -- the year this row represents for this actor
+)
+WITH (
+    format = 'PARQUET', -- setting data format as PARQUET
+    partitioning = ARRAY['current_year']  -- partitioning this data based on current_year
+)
