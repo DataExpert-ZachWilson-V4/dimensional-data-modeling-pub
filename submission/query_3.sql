941642a0bd3243ec075829767332df6c89fb85cd
RaniaAlzahrani <168978591+RaniaAlzahrani@users.noreply.github.com>
2024-05-22 00:16:09 +0300

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,7 +1,7 @@
 
 -- Create the table to track the historical data of actors
 CREATE OR REPLACE TABLE raniasalzahrani.actors_history_scd (
-    actor_id INT,  -- Unique identifier for each actor
+    actor_id VARCHAR,  -- Unique identifier for each actor
     quality_class VARCHAR(10),  -- Quality classification of the actor based on average rating
     is_active BOOLEAN,  -- Indicates if the actor is currently active
     start_date DATE,  -- The start date for the period this record is valid
