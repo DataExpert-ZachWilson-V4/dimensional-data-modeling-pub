45c8726c5324c6585e815ff9bc5eb3c87f7dbfd3
SteveHut <stevith@gmail.com>
2024-05-20 20:52:50 -0400

    removed REDUCE cumulative average calculation

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -22,46 +22,28 @@ this_year AS (
         film_id
         )
       ) as films,
-  year
+    CASE
+        WHEN AVG(rating) > 8 THEN 'star'
+        WHEN AVG(rating) > 7 AND AVG(rating) <= 8 THEN 'good'
+        WHEN AVG(rating) > 6 AND AVG(rating) <= 7 THEN 'average'
+        WHEN AVG(rating)  <= 6 THEN 'bad'
+    END as quality_class,
+    year
   FROM steve_hut.actor_films
   WHERE year = 1914 -- increment by 1 with each pass.
   GROUP BY actor, actor_id, year
-),
--- Create cumulative results after JOINING last_year and this_year
-cumulative_this_year AS (
+)
+-- insert cumulative results after JOINING last_year and this_year
   SELECT
       COALESCE(ly.actor, ty.actor) AS actor,
       COALESCE(ly.actor_id, ty.actor_id) AS actor_id,
       CASE
-        WHEN ty.films IS NULL THEN CAST(ly.films AS ARRAY(ROW(film VARCHAR, votes INTEGER, rating DOUBLE, film_id VARCHAR)))
-        WHEN ty.films IS NOT NULL AND ly.films IS NULL THEN CAST(ty.films AS ARRAY(ROW(film VARCHAR, votes INTEGER, rating DOUBLE, film_id VARCHAR)))
-        WHEN ty.films IS NOT NULL AND ly.films IS NOT NULL THEN CAST(ty.films || ly.films AS ARRAY(ROW(film VARCHAR, votes INTEGER, rating DOUBLE, film_id VARCHAR)))
+        WHEN ty.films IS NULL THEN ly.films
+        WHEN ty.films IS NOT NULL AND ly.films IS NULL THEN ty.films
+        WHEN ty.films IS NOT NULL AND ly.films IS NOT NULL THEN ty.films || ly.films
       END AS films,
-      ty.year IS NOT NULL AS is_active,
+      COALESCE(ty.quality_class, ly.quality_class) as quality_class,
+      ty.actor_id IS NOT NULL AS is_active,
       COALESCE(ty.year, ly.current_year + 1) AS current_year
   FROM last_year ly
   FULL OUTER JOIN this_year ty ON ty.actor_id = ly.actor_id
-)
--- Insert results into actors table, after calculating quality_class (average rating) based on cumulative_this_year films.
-SELECT
-  cty.actor,
-  cty.actor_id,
-  cty.films,
-  CASE
-    WHEN REDUCE(cty.films, CAST(ROW(0.0, 0) AS ROW(sum DOUBLE, count INTEGER)), (s, f) ->
-      CAST(ROW(f.rating + s.sum, s.count + 1) AS ROW(sum DOUBLE, count INTEGER)),
-      s -> IF(s.count = 0, NULL, s.sum / s.count)) > 8 THEN 'star'
-    WHEN REDUCE(cty.films, CAST(ROW(0.0, 0) AS ROW(sum DOUBLE, count INTEGER)), (s, f) ->
-      CAST(ROW(f.rating + s.sum, s.count + 1) AS ROW(sum DOUBLE, count INTEGER)),
-      s -> IF(s.count = 0, NULL, s.sum / s.count)) > 7 THEN 'good'
-    WHEN REDUCE(cty.films, CAST(ROW(0.0, 0) AS ROW(sum DOUBLE, count INTEGER)), (s, f) ->
-      CAST(ROW(f.rating + s.sum, s.count + 1) AS ROW(sum DOUBLE, count INTEGER)),
-      s -> IF(s.count = 0, NULL, s.sum / s.count)) > 6 THEN 'average'
-    WHEN REDUCE(cty.films, CAST(ROW(0.0, 0) AS ROW(sum DOUBLE, count INTEGER)), (s, f) ->
-      CAST(ROW(f.rating + s.sum, s.count + 1) AS ROW(sum DOUBLE, count INTEGER)),
-      s -> IF(s.count = 0, NULL, s.sum / s.count)) <= 6 THEN 'bad'
-  END AS quality_class,
-  cty.is_active,
-  cty.current_year
-FROM cumulative_this_year cty
-
