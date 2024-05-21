2e04383804cb0ded01d06333e3a7ec799365357e
hdamerla <harshithadamerla@gmail.com>
2024-05-21 12:21:27 -0500

    Update query_1.sql
    Cleaning code based on feedback from LLM

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,19 +1,25 @@
--- query_1
+-- HW1 query_1
+/* hdamerla 
+creating a table ased on the requirements.
+Even though year is not mentioned in the query1, including it basedon the feedback from LLM*/
 
-CREATE TABLE actors (
+
+--creating table statement and columns based on the requirements provided 
+CREATE TABLE hdamerla.actors ( 
     actor VARCHAR,
     actor_id VARCHAR,
-    films ARRAY(ROW(
+    films ARRAY(ROW(  -- creation of films array
       film VARCHAR,
       votes INTEGER,
       rating DOUBLE,
-      film_id VARCHAR
+      film_id VARCHAR,
+      year INTEGER
       )),
     quality_class VARCHAR,
     is_active BOOLEAN,
     current_year INTEGER
 )
-WITH (
+WITH ( --format and partition specification
   format = 'PARQUET',
   partitioning = ARRAY['current_year']  
 )
