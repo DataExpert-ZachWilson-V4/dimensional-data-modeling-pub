7c86af244b57a1e2291be52a3a0909e8b522c9f4
denzelbrown <denzelbrown@icloud.com>
2024-05-21 18:16:19 -0500

    Update query_1.sql

---
+++ submission/query_1.sql
@@ -0,0 +1,17 @@
+CREATE TABLE denzelbrown.actors (
+  actor VARCHAR,
+  actorid VARCHAR,
+  films ARRAY (
+    ROW (
+      film VARCHAR,
+      votes INTEGER,
+      rating DECIMAL,
+      filmid VARCHAR
+      )
+  ),
+  quality_class VARCHAR,
+  is_active BOOLEAN,
+  current_year INTEGER
+  )
+  WITH
+    (FORMAT = 'PARQUET', partitioning = ARRAY['current_year'])
