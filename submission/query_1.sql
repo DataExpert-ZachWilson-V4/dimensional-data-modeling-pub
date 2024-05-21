69f238835068aef22683bb6a6a8fc78cf3248f23
RaniaAlzahrani <168978591+RaniaAlzahrani@users.noreply.github.com>
2024-05-21 23:47:48 +0300

    Update query_1.sql

---
+++ submission/query_1.sql
@@ -0,0 +1,22 @@
+
+-- Create table
+CREATE TABLE raniasalzahrani.actors (
+    actor_id INT,
+    actor VARCHAR,
+    films ARRAY(
+        ROW(
+            film VARCHAR,
+            votes INT,
+            rating DOUBLE,
+            film_id INT
+        )
+    ),
+    quality_class VARCHAR,
+    is_active BOOLEAN,
+    current_year INT
+
+)
+WITH (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['current_year']
+)
