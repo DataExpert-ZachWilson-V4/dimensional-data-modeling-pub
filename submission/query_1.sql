c5a594911880a78f8b12803b63969a25c1be339b
sravan <47715988+sravanml@users.noreply.github.com>
2024-05-21 11:28:02 -0500

    query_1.sql

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,19 +1,17 @@
-CREATE OR REPLACE TABLE sravan.actors (
-    actor VARCHAR,       -- The name of the actor
-    actor_id VARCHAR,    -- A unique identifier for the actor
-    films ARRAY(         -- An array of structs representing the films the actor has worked in
-        ROW(
-            film VARCHAR,    -- The name of the film
-            votes INTEGER,   -- The number of votes the film has received
-            rating DOUBLE,   -- The rating of the film
-            film_id VARCHAR  -- A unique identifier for the film
-        )
-    ),
-    quality_class VARCHAR,  -- A categorization of the actor's films based on their average rating
-    is_active BOOLEAN,      -- A flag indicating if the actor is currently active in the film industry (making films this year)
-    current_year INTEGER    -- The current year for which the actor's data is recorded
+CREATE TABLE sravan.actors (
+  actor_id VARCHAR,
+  actor VARCHAR,
+  films ARRAY(ROW(
+    film VARCHAR,
+    votes INTEGER,
+    rating DOUBLE,
+    film_id VARCHAR
+  )),
+  quality_class VARCHAR,
+  is_active BOOLEAN,
+  current_year INTEGER
 )
 WITH (
-    FORMAT = 'PARQUET',    -- The table will be stored in Parquet format
-    partitioning = ARRAY['current_year'] -- The table will be partitioned by the 'current_year' column
+  FORMAT = 'PARQUET',
+  partitioning = ARRAY['current_year']
 )
