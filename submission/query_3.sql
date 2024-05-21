9d419c35a7fcc6e251f3b6296ad8b77a46c66898
sravan <47715988+sravanml@users.noreply.github.com>
2024-05-21 11:47:46 -0500

    Solution query_3_modified.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,12 +1,13 @@
 --Q3 DDL statement to create the actors_history_scd table
 CREATE OR REPLACE TABLE sravan.actors_history_scd (
-  actor_id VARCHAR,
+  actor VARCHAR,
   quality_class VARCHAR,
   is_active BOOLEAN,
   start_date INTEGER,
-  end_date INTEGER
+  end_date INTEGER,
+  current_year INTEGER
 )
 WITH (
   FORMAT = 'PARQUET',
-  PARTITIONING = ARRAY ['current_year']  -- Partitioned by 'current_year' for efficient time-based analysis
+  PARTITIONING = ARRAY ['current_year'] 
 )
