8013006db8380cf2f913a65ad9bbb0b07d6d4c63
Donny <168041002+xMidnightSoun@users.noreply.github.com>
2024-05-21 13:37:11 -0500

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,13 +1,23 @@
-CREATE OR REPLACE TABLE dswills94.actors_history_scd (
-	actor VARCHAR,
-	actor_id VARCHAR,
-	quality_class VARCHAR,
-	is_active BOOLEAN,
-	start_date INTEGER, --this is the SCD Type 2 for idenmpotency
-	end_date INTEGER, --this is the SCD Type 2 for idenmpotency
-	current_year INTEGER
+CREATE TABLE dswills94.actors_history_scd (
+--generate SCD table of actor history data
+actor VARCHAR,
+--name of actor
+actor_id VARCHAR,
+--id of actor primary key
+quality_class VARCHAR,
+--qualifier of film quality
+is_active BOOLEAN,
+--active flag
+start_date INTEGER,
+--this is the SCD Type 2 for idenmpotency
+end_date INTEGER,
+--this is the SCD Type 2 for idenmpotency
+current_year INTEGER
+--current year
 )
 WITH (
 	format = 'PARQUET',
-	partitioning = ARRAY['current_year']
+--usualy format to handle large data
+partitioning = ARRAY['current_year']
+--temporal compoenent by current year
 )
