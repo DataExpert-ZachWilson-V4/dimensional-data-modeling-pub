390a70e92d33ffa7a6aae6d4d85add2d17d4e8df
RaniaAlzahrani <168978591+RaniaAlzahrani@users.noreply.github.com>
2024-05-22 00:27:30 +0300

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,12 +1,14 @@
 
 -- Create the table to track the historical data of actors
 CREATE OR REPLACE TABLE raniasalzahrani.actors_history_scd (
-    actor_id VARCHAR,  -- Unique identifier for each actor
+    current_year INTEGER  -- The year this record pertains to
+    actor VARCHAR,  -- Stores the actor's name
     quality_class VARCHAR(10),  -- Quality classification of the actor based on average rating
     is_active BOOLEAN,  -- Indicates if the actor is currently active
     start_date DATE,  -- The start date for the period this record is valid
     end_date DATE  -- The end date for the period this record is valid (9999-12-31 for current records)
 )
 WITH (
-    FORMAT = 'PARQUET'  -- Store the table in Parquet format for efficient querying and storage
+  FORMAT = 'PARQUET',  -- Store the table in Parquet format for efficient querying and storage
+  partitioning = ARRAY ['current_year']  -- Partitioned by 'current_year' for efficient time-based analysis
 )
