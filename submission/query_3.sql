e17bdab28c762475273f715b8ba1fe77e99b0f5e
sravan <47715988+sravanml@users.noreply.github.com>
2024-05-21 11:42:56 -0500

    Solution for query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,11 +1,10 @@
 --Q3 DDL statement to create the actors_history_scd table
 CREATE OR REPLACE TABLE sravan.actors_history_scd (
-  actor VARCHAR,            -- Stores the actor's name
-  quality_class VARCHAR,    -- Categorical rating based on average rating in the most recent year
-  is_active BOOLEAN,        -- Indicates if the actor is currently active
-  start_date INTEGER,       -- Marks the beginning of a particular state (quality_class/is_active)
-  end_date INTEGER,         -- Signifies the end of a particular state
-  current_year INTEGER      -- The year this record pertains to
+  actor_id VARCHAR,
+  quality_class VARCHAR,
+  is_active BOOLEAN,
+  start_date INTEGER,
+  end_date INTEGER
 )
 WITH (
   FORMAT = 'PARQUET',
