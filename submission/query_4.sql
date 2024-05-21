699aea8add05cb25b62534c9644401a6a0b5a60c
hdamerla <harshithadamerla@gmail.com>
2024-05-21 15:12:51 -0500

    Update query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -9,12 +9,13 @@ SELECT
   actor_id,
   quality_class,
   CASE WHEN is_active THEN 1 ELSE 0 END AS is_active,
-  CASE WHEN  LAG(is_active, 1) OVER (PARTITION BY actor ORDER BY current_year) THEN 1 ELSE 0 END AS is_active_last_year,
+  CASE WHEN  LAG(is_active, 1) OVER (PARTITION BY actor ORDER BY current_year) THEN 1 ELSE 0 END AS is_active_last_year, --actor active previous year
   LAG(quality_class, 1) OVER (PARTITION BY actor_id ORDER BY current_year) AS quality_class_last_year,
   current_year 
 FROM hdamerla.actors
 ),
-streaked AS (
+
+  streaked AS (
 SELECT
   *,
   SUM(
@@ -31,7 +32,7 @@ SELECT
   actor,
   actor_id,
   quality_class,
-  MAX(is_active) = 1 as is_active,
+  MAX(is_active) = 1 as is_active, -- in lagged CTE boolean is converted to INT like in the labs/lecture
   MIN(current_year) as start_date,
   MAX(current_year) as end_date,
   2021 as current_year
