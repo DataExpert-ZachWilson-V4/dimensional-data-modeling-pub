69d42d0dd06449cf9f2adb8e6163ab1c203b550b
hdamerla <harshithadamerla@gmail.com>
2024-05-21 13:19:33 -0500

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,9 +1,9 @@
 --query_3
 /* DDL to create actors_history_scd table */
 
-CREATE OR REPLACE TABLE hdamerla.actors_history_scd (
-  actor VARCHAR,         -- 'Stores the actor's name. 
-  actor_id VARCHAR,      -- Unique identifier for each actor.
+CREATE OR REPLACE TABLE actors_history_scd (
+  actor VARCHAR(255),         -- 'Stores the actor's name. 
+  actor_id VARCHAR(255),      -- Unique identifier for each actor.
   quality_class VARCHAR, -- Categorical rating based on average rating in the most recent year.
   is_active BOOLEAN,     -- Indicates if the actor is currently active, based on making films this year.
   start_date INTEGER,    -- Marks the beginning of a particular state (quality_class/is_active). Integral in Type 2 SCD to track changes over time.
