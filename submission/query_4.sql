17fd3c35751d3670b3032c024fdcb3a87b106751
RaniaAlzahrani <168978591+RaniaAlzahrani@users.noreply.github.com>
2024-05-21 23:50:25 +0300

    Update query_4.sql

---
+++ submission/query_4.sql
@@ -0,0 +1,27 @@
+INSERT INTO raniasalzahrani.actors_history_scd (
+    actor_id,
+    quality_class,
+    is_active,
+    start_date,
+    end_date
+)
+WITH ranked_actors AS (
+    -- Create a CTE to generate the start and end dates for each actor's record
+    SELECT 
+        actor_id,
+        quality_class,
+        is_active,
+        -- Calculate the start date as the first day of the current year
+        DATE_TRUNC('day', CAST(CAST(current_year AS VARCHAR) || '-01-01' AS DATE)) AS start_date,
+        -- Use the LEAD function to get the start date of the next record as the end date of the current record
+        LEAD(DATE_TRUNC('day', CAST(CAST(current_year AS VARCHAR) || '-01-01' AS DATE)), 1) OVER (PARTITION BY actor_id ORDER BY current_year) AS next_start_date
+    FROM raniasalzahrani.actors
+)
+SELECT 
+    actor_id,
+    quality_class,
+    is_active,
+    start_date,
+    -- Set the end date to one day before the next start date, or to 9999-12-31 if it's the current record
+    COALESCE(next_start_date - INTERVAL '1' DAY, DATE '9999-12-31') AS end_date
+FROM ranked_actors
