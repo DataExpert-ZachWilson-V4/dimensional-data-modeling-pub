f3c9c82d36de042c98e4cc5774ec2515c4aa9696
rishav09 <rishavsapahia@gmail.com>
2024-05-21 22:27:12 -0400

    First 4 queries

---
+++ submission/query_3.sql
@@ -0,0 +1,15 @@
+CREATE TABLE 
+    human.actors_history_scd(
+    actor_id VARCHAR,
+    actor VARCHAR,
+    quality_class VARCHAR,
+    is_active BOOLEAN,
+    current_year INTEGER,
+    start_date DATE,
+    end_date DATE
+    
+)
+WITH (
+  format = 'PARQUET',
+  partitioning = ARRAY['current_year']
+)
