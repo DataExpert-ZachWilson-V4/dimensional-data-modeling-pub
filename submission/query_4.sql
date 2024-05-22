ad2f153790ddecb371fb364009e04da1943eddec
bucketz-build <134549544+bucketz-build@users.noreply.github.com>
2024-05-22 00:16:26 -0400

    Update query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -1,10 +1,46 @@
-INSERT INTO actors_history_scd (actor_id, quality_class, is_active, start_date, end_date)
-SELECT 
-    actor_id,
-    quality_class,
-    is_active,
+INSERT INTO actors_history_scd
+
+WITH previous_year AS (
+    SELECT
+        actor,
+        is_active,
+        quality_class,
+        LAG(is_active) OVER (
+            PARTITION BY actor
+            ORDER BY current_year
+        ) AS is_active_last_year,
+        LAG(quality_class) OVER (
+            PARTITION BY actor
+            ORDER BY current_year
+        ) AS quality_class_last_year,
+        current_year
+    FROM actors
+    WHERE current_year <= 1984
+),
+
+changed AS (
+    SELECT
+        *,
+        --  if is_active OR quality_class has changed
+        SUM(
+            CASE
+                WHEN is_active <> is_active_last_year OR quality_class <> quality_class_last_year THEN 1
+                ELSE 0
+            END
+        ) OVER (
+            PARTITION BY actor
+            ORDER BY
+                current_year
+        ) AS changed_identifier
+    FROM previous_year
+)
+
+SELECT
+    actor,
+    MAX(quality_class) AS quality_class,
+    MAX(is_active) AS is_active,
     MIN(current_year) AS start_date,
-    MAX(current_year) AS end_date
-FROM actors
-GROUP BY actor_id, quality_class, is_active
-ORDER BY actor_id, start_date;
+    MAX(current_year) AS end_date,
+    1984 AS current_year
+FROM changed
+GROUP BY actor, changed_identifier
