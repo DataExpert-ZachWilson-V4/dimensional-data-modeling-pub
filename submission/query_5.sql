730db196e9f56ed4b5e29180950e3b83aa76db4b
bucketz-build <134549544+bucketz-build@users.noreply.github.com>
2024-05-22 00:44:55 -0400

    Update query_5.sql

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -2,25 +2,18 @@
 
 WITH current_year_data AS (
     SELECT
-        actor_id,
-        quality_class,
-        is_active,
-        current_year
+       *8
     FROM actors
     WHERE current_year = 1984
 ),
 previous_scd_data AS (
     SELECT 
-        actor_id,
-        quality_class,
-        is_active,
-        start_date,
-        end_date
+       *
     FROM actors_history_scd
 )
 
 SELECT 
-    actor_id,
+    actor
     quality_class,
     is_active,
     current_year AS start_date,
@@ -35,25 +28,25 @@ WHERE NOT EXISTS (
 )
 UNION ALL
 SELECT 
-    actor_id,
+    actor
     quality_class,
     is_active,
     start_date,
     end_date
 FROM previous_scd_data
-WHERE actor_id NOT IN (
-    SELECT actor_id
+WHERE actor NOT IN (
+    SELECT actor
     FROM current_year_data
 )
 UNION ALL
 SELECT 
-    current_year_data.actor_id,
+    current_year_data.actor
     current_year_data.quality_class,
     current_year_data.is_active,
     previous_scd_data.start_date,
     current_year - 1 AS end_date
 FROM current_year_data
 JOIN previous_scd_data
-ON current_year_data.actor_id = previous_scd_data.actor_id
+ON current_year_data.actor = previous_scd_data.actor
 AND current_year_data.quality_class != previous_scd_data.quality_class
 OR current_year_data.is_active != previous_scd_data.is_active
