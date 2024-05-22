1ea602bb61470c0679ad169878f0d8aa5bc14847
bucketz-build <134549544+bucketz-build@users.noreply.github.com>
2024-05-21 23:49:58 -0400

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,1 +1,16 @@
 CREATE TABLE actors_history_scd
+
+(
+    
+  quality_class VARCHAR,
+    is_active BOOLEAN,
+    current_year INT
+  start_date INTEGER, 
+  end_date INTEGER
+)
+
+WITH
+  (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['current_year']
+  )
