80e262e15e74b2ab20a7379e2c6151a98a326cbe
Tammy Tian <tammytian@hey.com>
2024-05-22 19:04:50 -0700

    add coalesce to the start and end date and get final table

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -7,7 +7,7 @@ WITH last_year_scd AS (
     *
   FROM
     ttian45759.actors_history_scd 
-  WHERE current_year = 2005
+  WHERE current_year = 2021
 ),
 
 current_year AS (
@@ -15,9 +15,11 @@ current_year AS (
     *
   FROM
     ttian45759.actors
-  WHERE current_year = 2006
+  WHERE current_year = 2022
 ),
 
+
+-- Join the two tables and decide if there are changes
 row_reconciliation as (
   SELECT
     COALESCE(ly.actor, cy.actor) AS actor,
@@ -26,17 +28,100 @@ row_reconciliation as (
     cy.quality_class as current_year_quality_class,
     ly.is_active as last_year_is_active,
     cy.is_active as current_year_is_active,
+    -- need to coalesce the if there are new values
+    COALESCE(ly.start_date , cy.current_year) as start_date,
+    COALESCE(ly.end_date, cy.current_year) as end_date,
     CASE
       WHEN (ly.is_active <> cy.is_active OR ly.quality_class <> cy.quality_class) THEN 1
       WHEN (ly.is_active = cy.is_active AND ly.quality_class = cy.quality_class) THEN 0
     END AS did_change,
-    2006 AS current_year
+    2022 AS current_year
   FROM
     last_year_scd ly
   FULL OUTER JOIN current_year cy
   ON ly.actor_id = cy.actor_id
   AND ly.current_year + 1 = cy.current_year
+),
+
+
+-- Define a CTE for changes, similar to the example solution.
+changes AS (
+  SELECT
+    actor,
+    current_year,
+    CASE
+      WHEN did_change = 0
+        THEN ARRAY [
+          CAST(
+            ROW (
+              last_year_quality_class,
+              last_year_is_active,
+              start_date,
+              end_date + 1
+            ) AS ROW (
+              quality_class VARCHAR,
+              is_active BOOLEAN,
+              start_date INTEGER,
+              end_date INTEGER
+              )
+            )
+        ]
+      WHEN did_change = 1
+        THEN ARRAY [
+          CAST(
+            ROW (
+              last_year_quality_class,
+              last_year_is_active,
+              start_date,
+              end_date
+            ) AS ROW (
+              quality_class VARCHAR,
+              is_active BOOLEAN,
+              start_date INTEGER,
+              end_date INTEGER
+            )
+          ),
+          CAST(
+            ROW (
+              current_year_quality_class,
+              current_year_is_active,
+              current_year,
+              current_year
+            ) AS ROW (
+              quality_class VARCHAR,
+              is_active BOOLEAN,
+              start_date INTEGER,
+              end_date INTEGER
+            )	
+          )
+        ]
+      WHEN did_change IS NULL
+        THEN ARRAY [
+          CAST(
+            ROW (
+              COALESCE(last_year_quality_class, current_year_quality_class),
+              COALESCE(last_year_is_active, current_year_is_active),
+              start_date,
+              end_date
+            ) AS ROW (
+              quality_class VARCHAR,
+              is_active BOOLEAN,
+              start_date INTEGER,
+              end_date INTEGER
+              )
+            )
+        ]
+    END AS change_array
+  FROM row_reconciliation
 )
 
 
-select * from row_reconciliation where did_change = 0
+  SELECT
+    actor,
+    arr.quality_class,
+    arr.is_active,
+    arr.start_date,
+    arr.end_date,
+    current_year
+  FROM changes
+  CROSS JOIN UNNEST(change_array) AS arr
